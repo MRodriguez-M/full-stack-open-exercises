9e6d50325597935c01ffeef9be013f601acb804a
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-07-12 17:43:13 -0600

    Exercise 1.10 complete

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -12,13 +12,27 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.total}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.positive} %</p>
+      <StatisticLine text='good' value={props.good} />
+      <StatisticLine text='neutral' value={props.neutral} />
+      <StatisticLine text='bad' value={props.bad} />
+      <StatisticLine text='all' value={props.total} />
+      <StatisticLine text='average' value={props.average} />
+      <StatisticLine text='positive' value={props.positive} />
     </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleClick}>{props.name}</button>
+    </div>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <p>{props.text} {props.value}</p>
   )
 }
 
@@ -54,16 +68,16 @@ function App() {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
-      <Statistics 
-      good={good}
-      neutral={neutral}
-      bad={bad}
-      total={total}
-      average={average}
-      positive={positive}
+      <Button handleClick={handleGoodClick} name='good' />
+      <Button handleClick={handleNeutralClick} name='neutral' />
+      <Button handleClick={handleBadClick} name='bad' />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad} 
+        total={total}
+        average={average}
+        positive={positive}
       />
     </div>
   )
