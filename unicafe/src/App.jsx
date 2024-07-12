13b6cbabdc5e87f71c3cb3334c8a004b044e37bf
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-07-11 19:46:12 -0600

    Exercise 1.7 completed

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   const handleGoodClick = () => {
     setGood(() => good + 1)
+    setTotal(() => total + 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(() => neutral + 1)
+    setTotal(() => total + 1)
   }
   const handleBadClick = () => {
     setBad(() => bad + 1)
+    setTotal(() => total + 1)
   }
+
+  useEffect(() => {
+    if(total !== 0) {
+      setAverage((good-bad) / total)
+      setPositive((good/total) * 100)
+    }
+  }, [total])
 
   return (
     <div>
@@ -26,6 +39,9 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
