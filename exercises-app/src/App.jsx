2c3c79f5f81ba9e7c0c98d398a27c2466bb7468d
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-07-21 22:48:54 -0600

    Exercise 2.5 complete

--- exercises-app/src/App.jsx
+++ exercises-app/src/App.jsx
@@ -1,46 +1,4 @@
-const Header = (props) => {
-  return (
-    <h2>{props.name}</h2>
-  )
-}
-
-const Part = (props) => {
-  return(
-    <p>{props.part} {props.exercises}</p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.course.parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const totalExercises = props.parts.reduce((total, currentNum) => {
-    return total + currentNum.exercises;
-  }, 0)
-
-  return (
-    <b>total of {totalExercises} exercises</b>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      {props.courses.map(course => 
-        <div>
-          <Header name={course.name} />
-          <Content course={course} />
-          <Total parts={course.parts} />
-        </div>
-      )}
-    </div>
-  )
-}
+import Course from "./Course"
 
 const App = () => {
   const courses = [
