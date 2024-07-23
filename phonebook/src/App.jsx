504122a5c9bc728b358d76cb9108396348333dcc
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-07-22 19:06:16 -0600

    Exercise 2.8 complete

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 function App() {
-  const [persons, setPersons] = useState([{name: 'Arto Hellas'}]);
+  const [persons, setPersons] = useState([{name: 'Arto Hellas', num: '040-1234567'}]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addContact = (event) => {
     event.preventDefault();
     const contactObj = {
       name: newName,
+      num: newNumber,
     }
 
     for(let i = 0; i < persons.length; i++) {
@@ -21,23 +23,29 @@ function App() {
     }
 
     setNewName('');
+    setNewNumber('');
   }
 
-  const handleContactInfo = (event) => {
+  const handleContactName = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleContactNumber = (event) => {
+    setNewNumber(event.target.value);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addContact}>
-        <div>name: <input onChange={handleContactInfo} value={newName}/></div>
+        <div>name: <input onChange={handleContactName} value={newName}/></div>
+        <div>number: <input onChange={handleContactNumber} value={newNumber}/></div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p>{person.name}</p>)}
+      {persons.map(person => <p>{person.name} {person.num}</p>)}
     </div>
   )
 }
