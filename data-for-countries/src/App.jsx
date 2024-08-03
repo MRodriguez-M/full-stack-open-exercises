c0ef1f9b691f78d64b926352cb01dd58acf21531
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-08-03 17:52:39 -0600

    Exercise 2.19 complete

--- data-for-countries/src/App.jsx
+++ data-for-countries/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   const [country, setCountry] = useState('');
   const [countryList, setCountryList] = useState([]);
   const [filterList, setFilterList] = useState([]);
+  const [showClicked, setShowClicked] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const handleSearch = (e) => {
     setCountry(e.target.value);
@@ -25,16 +27,25 @@ function App() {
         return <p>Too many matches, specify another filter</p>
       }
       else if(filterList.length === 1) {
-        console.log(filterList)
         return <Country country={filterList[0]}/>
       }
       else {
-        return filterList.map(c => <p>{c.name.common}</p>)
+        return filterList.map(c =>
+          <div>
+            <p>{c.name.common}</p>
+            <button onClick={() => showCountryInfo(filterList.indexOf(c))}>show</button>
+          </div>
+        )
       }
     }
     else if (filterList.length === 0) {
       return null
     }
+  }
+
+  const showCountryInfo = (index) => {
+    setShowClicked(true);
+    setSelectedIndex(index);
   }
 
   useEffect(() => {
@@ -51,13 +62,14 @@ function App() {
       tempArr = [];
     }
     setFilterList(tempArr);
+    setShowClicked(false);
   }, [country])
 
   return (
     <div>
       find countries
       {(countryList.length > 0) && <input onChange={handleSearch} value={country} />}
-      {renderFilteredList()}
+      {(showClicked) ? <Country country={filterList[selectedIndex]}/> : renderFilteredList()}
     </div>
   )
 }
